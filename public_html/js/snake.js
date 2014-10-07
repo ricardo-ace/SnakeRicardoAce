5ef321bcb1e7e0c8a9d6e419caf151964da45b5e
ricardo acevedo <funnyaf123@gmail.com>
2014-10-07 15:02:57 -0700

    put background color and added new codes

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,2 +1,23 @@
+var snake;
 
+var context;
+var screenwidth;
+var screenheight;
 
+function gameinititialize() {
+    var canvas = document.getElementBy.Id("game-screen")
+    context = canvas.getContexts("2d");
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameloop() {
+    
+    
+}
+function gamedraw() {
+    context.fillstyle ="rgb(116,128,166)";
+}
