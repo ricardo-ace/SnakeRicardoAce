2f9b599944335fcd0bb2499d21c972da97caa658
ricardo acevedo <funnyaf123@gmail.com>
2014-10-10 15:01:36 -0700

    added more for snake inititialize

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,15 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
  
 gameInititialize();
+snakeInititialize();
 gameDraw();
+snakeDraw();
 
 function gameInititialize() {
     var canvas = document.getElementById("game-screen");
@@ -24,4 +28,26 @@ function gameLoop() {
 function gameDraw() {
     context.fillStyle ="rgb(116, 128, 166)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+function snakeInititialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = 0; index < snakeLength; index++) {
+        snake.push ( {
+            x:index,
+            y:0
+        });
+    }
+}
+
+function snakeDraw() {
+    for (var index = snakeLength - 1; index >= 0; index--){
+        context.fillstyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+function snakeUpdate() {
+    
 }
